87cb11612ed7de49f3e71c749ea9f2f3a83bb97a
armanheids <armanheids@gmail.com>
2024-06-06 15:32:53 +0330

    generate_salt added

--- hash_generator.py
+++ hash_generator.py
@@ -1,3 +1,6 @@
+import random
+
+
 class HashGenerator:
     sub_keys_path = "./constants/keys.txt"
     sbox_path = "./constants/sbox.txt"
@@ -11,7 +14,19 @@ class HashGenerator:
             sub_keys.extend([sub_key.strip() for sub_key in line.split(",")[:-1]])
         return sub_keys
 
-    def generate(plaintext):
+    def generate_salt(n):
+        key1 = ""
+
+        for i in range(n):
+            temp = str(random.randint(0, 1))
+            key1 += temp
+
+        return key1
+
+    def generate(plaintext, salt=None, work_factor=40):
+        if salt == None:
+            salt = HashGenerator.generate_salt(64)
+
         sub_keys = HashGenerator.keygen()
 
 
