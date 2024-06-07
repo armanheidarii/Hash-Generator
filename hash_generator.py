d5178cbf0cac221b2af3fcc567e50e9a79dcf400
armanheids <armanheids@gmail.com>
2024-06-08 01:12:19 +0330

    hash_generator modified

--- hash_generator.py
+++ hash_generator.py
@@ -170,4 +170,4 @@ plaintext = "0xa0a35e8ca7710"
 salt = "0xd62af4866aafe96e"
 work_factor = 13
 
-print(HashGenerator.generate(plaintext, salt=salt, work_factor=work_factor))
+HashGenerator.generate(plaintext, salt=salt, work_factor=work_factor)
